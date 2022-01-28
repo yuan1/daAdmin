562196c4c77bd003f1a56b70013d014ddda61710
limingyuan <mingyuan.li@linksfield.net>
2022-01-28 11:47:25 +0800

    init gateway

---
+++ gateway/src/main/java/cf/limingyuan/da/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package cf.limingyuan.da.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: lmy
+ * @date: 2021/12/31 13:37
+ * @desc:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
